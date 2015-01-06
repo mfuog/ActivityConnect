94f641558008bf0492f2a539fef1034697b1d3dc
Myrtha Fuog <myrtha@fuog.net>
2015-01-06 22:06:52 +0100

    Refactor CreateAdminService

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,7 +1,7 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: ENV['ADMIN_EMAIL']) do |user|
-    	user.username = ENV['ADMIN_USERNAME']
+    user = User.find_or_create_by!(username: ENV['ADMIN_USERNAME']) do |user|
+    	user.email = ENV['ADMIN_EMAIL']
     	user.first_name = ENV['ADMIN_FIRSTNAME']
     	user.last_name = ENV['ADMIN_LASTNAME']
       user.password = ENV['ADMIN_PASSWORD']
