fafd257116608bb0db55f02cd429244dff7c6e74
Myrtha Fuog <myrtha@fuog.net>
2015-01-10 22:34:12 +0100

    Refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ActiveRecord::Base
   enum role: [:user, :caretaker, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
-  def set_default_role
-    self.role ||= :user
-  end
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+  
+  def set_default_role
+    self.role ||= :user
+  end
 end
