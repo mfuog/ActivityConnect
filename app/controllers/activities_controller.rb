1fa19b63f38fc5be126704d0d599ae66d0880a56
Myrtha Fuog <myrtha@fuog.net>
2015-01-11 12:16:52 +0100

    Activities: Clean up

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,37 +2,56 @@ class ActivitiesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
+  respond_to :html, :json
 
+  # GET /activities
+  # GET /activities.json
   def index
     @activities = Activity.all
-  end
-
-  def show
-    @activity = Activity.find(params[:id])
-    @comments = @activity.comments.all
-    @comment = @activity.comments.build
-  end
-
-  def new
-    @activity = Activity.new
     respond_with(@activity)
   end
 
-  def edit
+  # GET /activities/:id
+  # GET /activities/:id.json
+  def show
+    @activity
+    @comments = @activity.comments.all
+    @comment = @activity.comments.build
+    @isAuthor = current_user == @activity.author
+    respond_with(@activity)
   end
 
+  # GET /activities/new
+  def new
+    @activity = Activity.new
+    respond_to :html
+  end
+
+  # GET /activities/:id/edit
+  def edit
+    unless current_user == @activity.author
+      redirect_to activity_path(@activity.id)
+    end
+    respond_to :html
+  end
+
+  # POST /activities
+  # POST /activities.json
   def create
     @activity = Activity.new(activity_params)
     flash[:notice] = 'Activity was successfully created.' if @activity.save
     respond_with(@activity)
   end
 
+  # PATCH/PUT /activities/:id
+  # PATCH/PUT /activities/:id.json
   def update
     flash[:notice] = 'Activity was successfully updated.' if @activity.update(activity_params)
     respond_with(@activity)
   end
 
+  # DELETE /activities/:id
+  # DELETE /activities/:id.json
   def destroy
     @activity.destroy
     respond_with(@activity)
@@ -43,6 +62,7 @@ class ActivitiesController < ApplicationController
       @activity = Activity.find(params[:id])
     end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def activity_params
       params.require(:activity).permit(:title, :description, :when, :author_id)
     end
