e20f819d3388c7d13b70c5fc1014214ea302d7b4
Myrtha Fuog <myrtha@fuog.net>
2015-01-11 18:04:23 +0100

    Remove unneeded Comment routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_comment, only: :show
 
   respond_to :html, :json
 
