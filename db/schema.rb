b17e6de9e872f6fd0941077412c6feedbe2612da
Myrtha Fuog <myrtha@fuog.net>
2015-01-11 12:34:35 +0100

    Add example comments to seed.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150110193445) do
+ActiveRecord::Schema.define(version: 20150111112255) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "title"
