b7d709557e1a4f8d1d5139418adfe5c2c8a2ba5e
Myrtha Fuog <myrtha@fuog.net>
2015-01-11 12:16:12 +0100

    Comments: Clean up and delete unneeded actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,40 +2,20 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
+  respond_to :html, :json
 
-  def index
-    @comments = Comment.all
-    respond_with(@comments)
-  end
-
+  # GET /comments/:id
+  # GET /comments/:id.json
   def show
-    respond_with(@comment)
+    @comment
   end
 
-  def new
-    @comment = Comment.new
-    respond_with(@comment)
-  end
-
-  def edit
-  end
-
+  # POST /comments
+  # POST /comments.json
   def create
-    puts comment_params
     @comment = Comment.new(comment_params)
     @comment.commenter_id = current_user.id if current_user
     flash[:notice] = 'Comment was successfully created.' if @comment.save
-    respond_with(@comment)
-  end
-
-  def update
-    flash[:notice] = 'Comment was successfully updated.' if @comment.update(comment_params)
-    respond_with(@comment)
-  end
-
-  def destroy
-    @comment.destroy
     respond_with(@comment)
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :activity
   belongs_to :commenter, class_name: "User"
+  validates :commenter, presence: true
+  validates :body, presence: true
 end
