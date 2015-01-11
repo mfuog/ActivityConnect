1265c1e764d12b4d133f68daf34093066bc5c5f6
Myrtha Fuog <myrtha@fuog.net>
2015-01-11 13:50:06 +0100

    Add newline

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :activity
   belongs_to :commenter, class_name: "User"
+  
   validates :commenter, presence: true
   validates :body, presence: true
 end
