2f4e92bf675ea6a9e824af0b5384544e54c9739d
Myrtha Fuog <myrtha@fuog.net>
2015-01-12 17:49:33 +0100

    Add forgotten file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     activities_path
@@ -13,13 +12,6 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_up_path_for(resource)
-    root_path#activity_path(1)
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name]
-    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
+    activities_path
   end
 end
