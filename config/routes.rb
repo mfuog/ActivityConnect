ce681e95013312b542ff45e6098630e2b8ee8c73
Myrtha Fuog <myrtha@fuog.net>
2015-01-12 17:47:16 +0100

    Extract overriding Devise User model into RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   root to: "visitors#index"
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   resources :users
   resources :activities
   resources :comments, only: [:create]
