2c581bddad70f0f27aeb6b6a90a1fa30051f9d66
Myrtha Fuog <myrtha@fuog.net>
2015-01-13 14:59:33 +0100

    Allow creating and editing a user's address

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,8 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name]
-    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
+    devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name, :address]
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, :address]
   end
 
   def sign_up_params
@@ -13,6 +13,7 @@ class RegistrationsController < Devise::RegistrationsController
           .permit(:first_name,
                   :last_name,
                   :email,
+                  :address,
                   :password,
                   :password_confirmation)
   end
@@ -22,6 +23,7 @@ class RegistrationsController < Devise::RegistrationsController
           .permit(:first_name,
                   :last_name,
                   :email,
+                  :address,
                   :password,
                   :password_confirmation,
                   :current_password)
