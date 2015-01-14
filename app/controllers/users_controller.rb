4e901184bbd138cb099bd79073049d0791e2baa5
Myrtha Fuog <myrtha@fuog.net>
2015-01-14 11:33:43 +0100

    Add route /profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,11 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    if params[:id]
+      @user = User.find(params[:id])
+    else
+      @user = current_user
+    end
   end
 
   def update
